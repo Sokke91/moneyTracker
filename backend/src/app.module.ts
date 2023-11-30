5f93468b96dd336e783c07a060e37dee2c5cc6b1
Jonas Angel <jonas.angel.ja@gmail.com>
2023-11-30 10:54:53 +0100

    scaffild users resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContractsModule } from './contracts/contracts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ContractsModule } from './contracts/contracts.module';
       entities: [],
     }),
     ContractsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
