b869bc6dbbab7f5adef90a62da52178c562a2dff
Jonas Angel <jonas.angel.ja@gmail.com>
2023-11-30 11:27:46 +0100

    impl account entity

--- backend/src/accounts/entities/account.entity.ts
+++ backend/src/accounts/entities/account.entity.ts
@@ -1,1 +1,22 @@
-export class Account {}
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Account {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({nullable: false})
+    name: string
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    // Relations
+
+    @ManyToOne(() => User, (user) => user.accounts)
+    user: User
+}
